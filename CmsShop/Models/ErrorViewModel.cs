481e213b55f33419e5ace24255ecc53445cbebf0
t220186 <m.bronzewsi@gmail.com>
2020-01-04 12:30:29 +0100

    Start rojektu cmsShop - sklep o funkjonalnosci zendowego bellodecor.pl ale  netcore - wyglad minimalistyczny praktycznie czysty bootstrap

---
+++ CmsShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CmsShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
