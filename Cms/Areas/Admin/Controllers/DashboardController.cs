a2e0c77b14ff8f82c202c0ce1da325cb495bdf2f
Michał Bronżewski <xcube@xcube.pl>
2020-01-05 14:30:53 +0100

    aktualizacja konfiguracji

---
+++ Cms/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Cms.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
